a5c6468f92247277525f73b6fc7b49fe949d993b
Marco <marco.zappani@gmail.com>
2021-11-07 12:19:44 +0100

    solved remote issue with menu, notes in menuIssue.txt

---
+++ my-app/src/render.js
@@ -0,0 +1,24 @@
+const { desktopCapturer } = require("electron");
+const { Menu } = require("@electron/remote");
+const videoElement = document.querySelector("video");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const videoSelectBtn = document.getElementById("videoSelectBtn");
+videoSelectBtn.onclick = getVideoSources;
+
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ["window", "screen"],
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map((source) => {
+      return {
+        label: source.name,
+        click: () => selectSource(source),
+      };
+    })
+  );
+
+  videoOptionsMenu.popup();
+}
